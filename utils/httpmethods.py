b3e607303685ed629c00edb1b49c31c32405e7db
Dimitry <dartahilles@mail.ru>
2025-08-31 14:19:55 +0400

    коррекция записи

--- utils/httpmethods.py
+++ utils/httpmethods.py
@@ -7,31 +7,39 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
-        result = requests.get(url,
-                              headers=HttpMethods.headers,
-                              cookies=HttpMethods.cookie)
+        result = requests.get(
+            url,
+            headers=HttpMethods.headers,
+            cookies=HttpMethods.cookie
+        )
         return result
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url,
-                               json=body,
-                               headers=HttpMethods.headers,
-                               cookies=HttpMethods.cookie)
+        result = requests.post(
+            url,
+            json=body,
+            headers=HttpMethods.headers,
+            cookies=HttpMethods.cookie
+        )
         return result
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url,
-                              json=body,
-                              headers=HttpMethods.headers,
-                              cookies=HttpMethods.cookie)
+        result = requests.put(
+            url,
+            json=body,
+            headers=HttpMethods.headers,
+            cookies=HttpMethods.cookie
+        )
         return result
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url,
-                                 json=body,
-                                 headers=HttpMethods.headers,
-                                 cookies=HttpMethods.cookie)
+        result = requests.delete(
+            url,
+            json=body,
+            headers=HttpMethods.headers,
+            cookies=HttpMethods.cookie
+        )
         return result
