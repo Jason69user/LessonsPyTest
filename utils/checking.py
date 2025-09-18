3759f106efcd385f455a3b233aad6bdc44098341
Dimitry <dartahilles@mail.ru>
2025-09-18 19:38:17 +0400

    убрал if/else

--- utils/checking.py
+++ utils/checking.py
@@ -1,3 +1,5 @@
+import json
+
 from requests import Response
 
 
@@ -6,8 +8,11 @@ class Checking:
     # проверяем статус-код запроса
     @staticmethod
     def check_status_code(response: Response, status_code):
-        assert status_code == response.status_code
-        if response.status_code == status_code:
-            print(f"Успешно. Статус-код = " + str(response.status_code))
-        else:
-            print("Провал. Статус-код = " + str(response.status_code))
+        assert status_code == response.status_code, print("Провал. Статус-код = " + str(response.status_code))
+        print(f"Успешно. Статус-код = " + str(response.status_code))
+
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
